01dbecb79667c32f2b8f08248db1b7acaf8290c5
Grizly <antoine.marie.dev@gmail.com>
2021-10-06 16:34:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="grid_container">
+        <div className="sidebar"><User/></div>
+          <div className="main">main</div>
+      </div>
+
+
+      
+    {/* react, material ui, react-html2pdf */}
+    
+
+    {/* compétences - skills */}
+    {/* langues - skills */}
+    {/* centre d'intérêts - interests */}
+
+    {/* colone de droite */}
+    {/* profil - profil */}
+    {/* formations - formations */}
+    {/* exp  - experiences */}
+
+    {/* light/darkmode*/}
+    {/* generation pdf */}
+
+    
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./User.css";
+import HomeIcon from '@mui/icons-material/Home';
+import PhoneIcon from '@mui/icons-material/Phone';
+import RoomIcon from '@mui/icons-material/Room';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import EmailIcon from '@mui/icons-material/Email';
+
+
+
+function User() {
+    return (
+    <div className="user">
+        <img src="./images/avatar.jpg" alt="" className="user__avatar"/>
+        <h1 className="user__name">Antoine MARIE</h1>
+        <p className="user__profession">Développeur</p>
+        <div className="user__infos">
+            <p className="user__info">
+                <HomeIcon /> 57 route national 62360 Isques</p>
+            <p className="user__info">
+                <PhoneIcon />  <a href="tel:+33645794249"> 0645794249</a></p>
+            <p className="user__info"> 
+               <EmailIcon /> <a href="mailto:antoine.marie.dev@gmail.com">antoine.marie.dev@gmail.com</a></p>
+            <p className="user__info"><CalendarTodayIcon />Date de naissance: 28 Septembre 1993</p>
+            <p className="user__info"><EmailIcon />llieu de naissance: Boulogne-sur-mer</p>
+        </div>
+    </div>
+    )
+}
+
+export default User;
