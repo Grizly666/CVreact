dedf155133959c29c61f853a4880dfb5404a2026
Antoine MARIE <antoine.marie62126@gmail.com>
2021-10-10 11:10:26 +0200

    ajout du darkmode plus pdf

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import User from './components/User';
 import Skills from './components/Skills';
 import Profil from './components/Profil';
 import FormationsExperiences from './components/FormationsExperiences';
+import DarkMode from './components/DarkMode';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import { Preview, print } from 'react-html2pdf';
 
 function App() {
+  const handleGenerateCv = () => {
+    let cvTemplate = document.getElementById("cv-print")
+    cvTemplate.setAttribute("style", "width:210mm !important")
+    cvTemplate.classList.add("cv-print")
+    document.body.classList.remove("dark")
+    setTimeout(() => {
+      print("cv", "cv-print")
+      cvTemplate.setAttribute("style", "width:100% !important")
+      cvTemplate.classList.remove("cv-print")
+    }, 300)
+  }
   return (
+    <Preview id={"cv-print"} class="cv-print">
     <div className="App">
 
       <div className="grid_container">
           <div className="sidebar">
+            <div className="action">
+              <DarkMode />
+              <button onClick={handleGenerateCv}>
+                <PictureAsPdfIcon /></button>
+            </div>
+            
             <User />
             <Skills />
           </div>
@@ -19,6 +40,7 @@ function App() {
             <FormationsExperiences />
             </div>
       </div>
+      
 
 
       
@@ -34,7 +56,7 @@ function App() {
     {/* generation pdf */}
 
     
-    </div>
+    </div></Preview>
   );
 }
 
